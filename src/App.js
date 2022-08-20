0cb5644e5423277a2601b35bb7ea598dde01f71f
k-gardn <rladnjs15@naver.com>
2022-08-20 16:41:56 +0900

    action creator를 이용한 수정

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 import React from "react";
-import { useSelector } from "react-redux"; // import 해주세요.
-import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { minusOne, plusOne } from "./redux/modules/counter";
+import { addNumber } from "./redux/modules/counter";
+import { minusNumber } from "./redux/modules/counter";
 
 const App = () => {
-  const dispatch = useDispatch(); // dispatch 함수 생성. 액션 객체를 리듀서로 보내는 '전달자'함수.
-  const number = useSelector((state) => state.counter.number); //리덕스 스토어를 조회. 화살표 함수에서 꺼낸 state라는 인자는 현재 프로젝트에 존재하는 모든 리덕스 모듈의 state 
-  // console.log(number);
+  const dispatch = useDispatch();
+  const [number, setNumber] = useState(0);
+
+  const globalNumber = useSelector((state) => state.counter.number);
+  
+  const onChangeHandler = (event) => {
+
+    const { value } = event.target; //객체 구조 분해 할당. 우측은 분해하고자 하는 객체, 좌측엔 상응하는 객체 프로퍼티의 '패턴'을 넣음.
+    // console.log(event.target); //<input type="number">
+		// event.target.value는 문자열 입니다.
+		// 이것을 숫자형으로 형변환해주기 위해서 +를 붙여 주었습니다.
+    setNumber(+value);
+  };
+
+  const onClickAddNumberHandler = () => {
+		// 5. Action creator를 dispatch 해주고, 그때 Action creator의 인자에 number를 넣어줍니다.
+    dispatch(addNumber(number));
+  };
+
+  const onClickMinusNumberHandler =() => {
+    dispatch(minusNumber(number));
+  }
+
   return (
     <div>
-      {number}
-      <button
-				// 이벤트 핸들러 추가
-        onClick={() => {
-					// 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달된다.
-          dispatch(plusOne()); 
-        }}
-      >
-				+ 1
-      </button>
-
-      <button
-        onClick={() => {
-					// 액션객체 디스패치
-          dispatch(minusOne());
-        }}
-      >
-        - 1
-      </button>
-    
+      <div>{globalNumber}</div>
+      <input type="number" onChange={onChangeHandler} />
+      <button onClick={onClickAddNumberHandler}>더하기</button>
+      <button onClick={onClickMinusNumberHandler}>빼기</button>
     </div>
   );
 };
-
 
 export default App;
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,44 +1,42 @@
-//액션 value를 상수들로 만들어 줍니다. 보통 이렇게 한곳에 모여있습니다.
-const PLUS_ONE ="PLUS_ONE"
-const MINUS_ONE = "MINUS_ONE"
+// Action Value
+const ADD_NUMBER = "ADD_NUMBER";
+const MINUS_NUMBER = "MINUS_NUMBER";
 
-// 추가된 코드 👇 - Action Creator를 만들어 줍니다. 
-export const plusOne = () => {
-    return{
-        type: PLUS_ONE,
-    }
-}
+// Action Creator
+export const addNumber = (payload) => {
+    return {
+      type: ADD_NUMBER,
+      payload: payload,
+    };
+  };
 
-export const minusOne = () => {
-    return{
-        type: MINUS_ONE,
-    }
-}
-
-
-//초기 상태값 // 객체{}안의 number라는 변수의 초기값을 0으로 설정.
-const initialState = {
-    number: 0,
+export const minusNumber = (payload) => {
+return {
+    type: MINUS_NUMBER,
+    payload: payload,
+};
 };
 
-// 리듀서(변화를 일으키는 함수) useState의 setNumber와 같은 역할.
-const counter = (state = initialState, action) => {
-    // console.log(action);
-    switch (action.type) {
-        case PLUS_ONE:
-            return {
-                number: state.number +1,
-            }
-        
-        case MINUS_ONE:
-            return {
-                number: state.number -1,
-            };
-
-        default:
-        return state;
-    }
+// Initial State
+const initialState = {
+    number: 0,
   };
-  
-  // 모듈파일에서는 리듀서를 export default 한다.
-  export default counter;
+
+// Reducer
+const counter = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_NUMBER:
+            return {
+                number: state.number + action.payload,
+            };
+        case MINUS_NUMBER:
+            return {
+                number: state.number - action.payload,
+            };
+        default:
+            return state;
+    }
+  };  
+
+// export default reducer
+export default counter;
